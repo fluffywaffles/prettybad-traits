c3692e7500580020ce99628b6fe2dae37934e20b
fluffywaffles <skorlir@gmail.com>
2018-09-05 22:23:18 -0500

    Bugfix/Major: don't cache target state! Use getters

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import {
   ᐅwhen,
   id,
   and,
+  get,
   has,
   not,
   bind,
   flip,
   fold,
+  push,
   update,
   method_of,
   define_property,
@@ -115,31 +117,66 @@ function _override_propagate (override) {
  * the propagating (nested) object is commuted.
  */
 export function TracksPropagation (keys) {
-  return flip(fold(TracksPropagation.track_key))(keys)
+  const tracking_symbol = TracksPropagation.tracking_symbol
+  return ᐅᶠ([
+    define_property.mut(tracking_symbol)(d.configurable ({ v: [] })),
+    flip(fold(TracksPropagation.track_key))(keys),
+  ])
 }
 
 define_properties.mut({
   tracking_symbol : d.nothing ({ v: Symbol(`tracking`) }),
+  add_to_tracking : d.nothing ({ v: _add_to_tracking }),
   track_key       : d.nothing ({ v: _track_key }),
 })(TracksPropagation)
+function _add_to_tracking ({ key, symbol }) {
+  return target => {
+    const tracked_symbol = TracksPropagation.tracking_symbol
+    return update(tracked_symbol)(push({ key, symbol }))(target)
+  }
+}
+
+/**
+ * `TracksPropagation.track_key` hides the value of a key under a private
+ * symbol, and replaces the key with a proxy getter that passes the
+ * parent's dynamic context (`this`) into the child object for
+ * propagation. Note that internal overrides are *mutative*, making them
+ * more efficient if a propagation-proxy getter is called many times.
+ */
 function _track_key (key) {
   return target => {
     console.assert(
       has(key)(target),
       `TracksPropagation: key (${key}) does not exist in target!`,
     )
-    const propagate = Propagators.propagate_up({ key, target })
-    const override_propagate = Propagates.override(propagate)
-    return ᐅwhen(has(key))(update(key)(override_propagate))(target)
+    const tracked_child = get(key)(target)
+    const symbol = Symbol(`tracked_child[${key}]`)
+    const proxy = create_propagate_proxy(Propagators.propagate_up)(symbol)
+    return ᐅwhen(has(key))(ᐅᶠ([
+      TracksPropagation.add_to_tracking({ key, symbol }),
+      define_properties.mut({
+        [symbol] : d.non_writable ({ v: tracked_child }),
+        [key]    : d.enumerable   ({ g: proxy }),
+      }),
+    ]))(target)
+  }
+}
+
+function create_propagate_proxy (Propagator) {
+  return function propagate_proxy_child (symbol) {
+    return function override_propagate_using_parent_context () {
+      const overrider = Propagator({ context: this, symbol })
+      return Propagates.override(overrider)(this[symbol])
+    }
   }
 }
 
 const Propagators = {
-  propagate_up: propagate_up_create,
+  propagate_up: create_propagate_up,
 }
-function propagate_up_create ({ key, target }) {
+function create_propagate_up ({ symbol, context }) {
   return function propagate_up (result) {
-    return update(key)(_ => result)(target)
+    return update(symbol)(_ => result)(context)
   }
 }
 
@@ -156,11 +193,20 @@ function override_internal (symbol) {
   }
 }
 
+import {
+  map,
+  types,
+  get_in,
+  reflex,
+  update_path,
+} from '@prettybad/util'
+
 export function test (suite) {
   function Tree () {
-    return TracksPropagation([`leaf`])({
-      leaf: Propagates(Leaf({})),
-      height: 100,
+    return TracksPropagation([`leaf`, `leaf2`])({
+      leaf   : Propagates(Leaf({})),
+      leaf2  : Propagates(Leaf({})),
+      height : 100,
     })
   }
 
@@ -177,23 +223,29 @@ export function test (suite) {
     })
   }
 
-  const tree = Tree()
-
-  return suite(`sanity checks`, {
-    'trees have green leaves': t => {
-      return t.eq(tree.height)(100)
-          && t.eq(tree.leaf.state.color)('green')
-    },
-    'they can turn brown': t => {
-      const turned = tree.leaf.turn()
-      return t.eq(turned.height)(100)
-          && t.eq(turned.leaf.state.color)('brown')
-    },
-    'and they can turn back again': t => {
-      const turned_twice
-        = tree.leaf.turn().leaf.turn()
-      return t.eq(turned_twice.height)(100)
-          && t.eq(turned_twice.leaf.state.color)('green')
-    },
-  })
+  return suite(`we all have pretty bad traits...`, [
+    t => t.suite(`Tree example`, {
+      'trees have green leaves': t => {
+        const tree = Tree()
+        return t.eq(tree.height)(100)
+            && t.eq(tree.leaf.state.color)('green')
+      },
+      'they can turn brown': t => {
+        const turned = Tree().leaf.turn()
+        return t.eq(turned.height)(100)
+            && t.eq(turned.leaf.state.color)('brown')
+      },
+      'and they can turn back again': t => {
+        const turned_twice
+          = Tree().leaf.turn().leaf.turn()
+        return t.eq(turned_twice.height)(100)
+            && t.eq(turned_twice.leaf.state.color)('green')
+      },
+      'and there is no caching or lossy updating': t => {
+        const lossless = Tree().leaf.turn().leaf2.turn()
+        return t.eq(lossless.leaf.state.color)('brown')
+            && t.eq(lossless.leaf2.state.color)('brown')
+      },
+    }),
+  ])
 }
